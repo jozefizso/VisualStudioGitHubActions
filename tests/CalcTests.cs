806dc34a3f53a11832c8a58411fc7de646a4a884
Jozef Izso <jozef.izso@gmail.com>
2020-03-29 12:22:40 +0200

    Add unit tests project for MathLib

---
+++ tests/CalcTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace MathLib.Tests
+{
+    [TestFixture]
+    public class CalcTests
+    {
+        [Test]
+        public void Constructor()
+        {
+            var calc = new Calc();
+
+            Assert.Pass();
+        }
+    }
+}
---
+++ tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MathLib.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MathLib")]
+[assembly: AssemblyCopyright("Copyright © 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("24a0d32f-cf93-45ac-bee1-3779221556c0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
